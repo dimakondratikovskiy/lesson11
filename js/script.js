0037c6d42b34e80396a60b2c87fc1de42903d02e
Dmitrij <96dima987@mail.ru>
2020-12-11 19:19:11 +0300

    myfirstcommit

---
+++ js/script.js
@@ -0,0 +1,86 @@
+//zadacha2
+let a1= 5 % 3,
+	 a2= 3 % 5,
+	 a3= 5 + '3',
+	 a4= '5' - 3,
+	 a5= 75 + 'кг',
+	 a6= 785 * 653,
+	 a7= 100 / 25,
+	 a8= 0 * 0,
+	 a9= 0 / 2,
+	 a10= 89 / 0,
+	 a11= 98 + 2,
+	 a12= 5 - 98,
+	 a13= (8 + 56 * 4) / 5,
+	 a14= (9 - 12) * 7 / (5 + 2),
+	 a15= +"123",
+	 a16= 1 || 0,
+	 a17= false || true,
+	 a18= true > 0;
+console.log(a1, typeof a1);
+console.log(a2, typeof a2);
+console.log(a3, typeof a3);
+console.log(a4, typeof a4);
+console.log(a5, typeof a5);
+console.log(a6, typeof a6);
+console.log(a7, typeof a7);
+console.log(a8, typeof a8);
+console.log(a9, typeof a9);
+console.log(a10, typeof a10);
+console.log(a11, typeof a11);
+console.log(a12, typeof a12);
+console.log(a13, typeof a13);
+console.log(a14, typeof a14);
+console.log(a15, typeof a15);
+console.log(a16, typeof a16);
+console.log(a17, typeof a17);
+console.log(a18, typeof a18);
+
+//zadacha3
+let height = 23,
+	   width = 10;
+let SPr = width * height;
+console.log(SPr, typeof SPr);
+
+//zadacha4
+let P= Math.PI,
+	 H= 10;
+let VCil = P * ((a7*a7)/4) * H;
+console.log(VCil, typeof VCil);
+
+//zadacha5
+let r = 5;
+let SKruga = P * (r*r);
+console.log(SKruga, typeof SKruga);
+
+//zadacha6
+let a = 5,
+	 b = 7,
+	 h = 10;
+let STrap = ((a + b)/2) * h;
+console.log(STrap, typeof STrap);
+
+//zadacha7
+let S= 2000000,
+	 p= 0.01,
+	 years = 5;
+let Pereplata = (S * p * years) + "руб.";
+console.log(Pereplata, typeof Pereplata);
+
+//zadacha 8
+let A= 8,
+	 B= 3;
+let x1 = (16 - A)/2 + B;
+let x2 = (A - 15*B)/(B - 6);
+let x3 = 23780/(1 + 2 + A + B);
+console.log(x1, x2, x3);
+
+
+
+
+
+
+
+
+
+
